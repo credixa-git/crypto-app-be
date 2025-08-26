c969e2314fdc839b70314d1d453008837786a5e1
Ayush Garg <ayush181000@gmail.com>
2025-08-26 22:58:18 +0530

    update env

--- config/appConfig.js
+++ config/appConfig.js
@@ -21,8 +21,8 @@ const AppConfig = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     ses: {
-      fromEmail: process.env.AWS_SES_FROM_EMAIL ?? "noreply@yourdomain.com",
-      fromName: process.env.AWS_SES_FROM_NAME ?? "Crypto Port",
+      fromEmail: process.env.AWS_SES_FROM_EMAIL ?? "noreply@credixa.xyz",
+      fromName: process.env.AWS_SES_FROM_NAME ?? "Credixa",
     },
     s3: {
       bucketName: process.env.AWS_S3_BUCKET_NAME,
